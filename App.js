0e18ecc1ada50827c50ef08df045902e4ae5d12c
ddjb77 <67380362+ddjb77@users.noreply.github.com>
2021-08-28 15:10:55 -0300

    app perfil

--- App.js
+++ App.js
@@ -1,21 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+class App extends Component {
+  render() {
+    let legenda = "Aluno da FATEC RUBENS LARA, atualmente cursa Sistemas para Internet, gosta da área de design e front-end";
+
+
+    return (
+      <View style={styles.container}>
+        <Text style={{ textAlign: 'center', fontSize: 30, paddingTop: 15 }}>
+          David de Jesus Bezerra
+        </Text>
+
+        <Image
+          source={{
+            uri:
+              'https://avatars.githubusercontent.com/u/67380362?v=4'
+          }}
+          style={{ width: 250, height: 250, borderRadius: 250 }}
+        />
+
+        <Text style={{ color: '#333', fontSize: 16, margin: 15, textAlign: 'justify' }}>{legenda}</Text>
+        <a target="_blank" href="https://github.com/ddjb77">Github</a>
+      </View>
+    );
+  }
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#bcbfcb',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+
 });
+
+export default App
